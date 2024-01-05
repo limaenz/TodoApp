26d066e81d2d958b9e1e7871fc3ad22aa76834e6
limaenz <enzolimafranca@gmail.com>
2024-01-05 00:24:49 -0300

    Ajustas regras de paginação das TODO e tratamentos de erro de conversao de objetos

--- src/server/repository/todo.ts
+++ src/server/repository/todo.ts
@@ -1,6 +1,7 @@
 import { read } from "fs";
 import { HttpNotFoundError } from "@server/infra/errors";
 import { createClient } from "@supabase/supabase-js";
+import { Todo, TodoSchema } from "@server/schema/todo";
 
 const supabaseUrl = process.env.SUPABASE_URL || "";
 const supabaseKey = process.env.SUPABASE_SECRET_KEY || "";
@@ -21,44 +22,35 @@ async function get({
     page,
     limit,
 }: TodoRepositoryGetParams = {}): Promise<TodoRepositoryGetOutput> {
-    const { data, error, count } = await supabase.from("todos").select("*", {
-        count: "exact",
-    });
+    const currentPage = page || 1;
+    const currentLimit = limit || 10;
+    const startIndex = (currentPage - 1) * currentLimit;
+    const endIndex = currentPage * currentLimit - 1;
+
+    const { data, error, count } = await supabase
+        .from("todos")
+        .select("*", {
+            count: "exact",
+        })
+        .range(startIndex, endIndex);
 
     if (error) throw new Error("Failed to fetch date");
 
-    const todos = data as Todo[];
+    const parsedData = TodoSchema.array().safeParse(data);
+
+    if (!parsedData.success) {
+        throw new Error("Failed to parse TODO from database");
+    }
+
+    const todos = parsedData.data;
     const total = count || todos.length;
+    const totalPages = Math.ceil(total / currentLimit);
 
     return {
         todos,
         total,
-        pages: 1,
+        pages: totalPages,
     };
-
-    // const currentPage = page || 1;
-    // const currentLimit = limit || 10;
-
-    // const todos: Todo[] = [
-    //     { id: "1", content: "Primeira TODO", date: new Date(), done: false },
-    //     { id: "2", content: "Segunda TODO", date: new Date(), done: false },
-    //     { id: "3", content: "Terceira TODO", date: new Date(), done: false },
-    //     { id: "4", content: "Quarto Teste", date: new Date(), done: false },
-    //     { id: "5", content: "Quinto Teste", date: new Date(), done: false },
-    //     { id: "6", content: "Sexto teste", date: new Date(), done: false },
-    // ];
-
-    // const ALL_TODOS = todos;
-    // const startIndex = (currentPage - 1) * currentLimit;
-    // const endIndex = currentPage * currentLimit;
-    // const paginatedTodos = ALL_TODOS.slice(startIndex, endIndex);
-    // const totalPages = Math.ceil(ALL_TODOS.length / currentLimit);
-
-    // return {
-    //     total: ALL_TODOS.length,
-    //     todos: paginatedTodos,
-    //     pages: totalPages,
-    // };
 }
 
 async function createdByContent(content: string): Promise<Todo> {
@@ -100,10 +92,3 @@ export const todoRepository = {
     toggleDone,
     deleteById,
 };
-
-interface Todo {
-    id: string;
-    content: string;
-    date: Date;
-    done: boolean;
-}
